d40eb1efe990e7847fa4357d35480b2c5c755dda
ShikshaFinder <janiharsh794@gmail.com>
2024-10-03 20:14:51 +0530

    h

--- src/pages/api/state.ts
+++ src/pages/api/state.ts
@@ -1,7 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import * as fs from 'fs';
-export const runtime = "edge";
 
 
 type Data = {

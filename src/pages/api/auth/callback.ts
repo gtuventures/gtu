67825d0bb38b414a67e675da3d16f919b5ef1086
ShikshaFinder <janiharsh794@gmail.com>
2024-10-03 21:09:12 +0530

    j

--- src/pages/api/auth/callback.ts
+++ src/pages/api/auth/callback.ts
@@ -1,18 +1,18 @@
-import { NextApiHandler } from "next";
-import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
-export const runtime = "edge";
+// import { NextApiHandler } from "next";
+// import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
+// export const runtime = "edge";
 
 
-const handler: NextApiHandler = async (req, res) => {
+// const handler: NextApiHandler = async (req, res) => {
   
-  const { code } = req.query;
+//   const { code } = req.query;
 
-  if (code) {
-    const supabase = createPagesServerClient({ req, res });
-    await supabase.auth.exchangeCodeForSession(String(code));
-  }
+//   if (code) {
+//     const supabase = createPagesServerClient({ req, res });
+//     await supabase.auth.exchangeCodeForSession(String(code));
+//   }
 
-  res.redirect("/");
-};
+//   res.redirect("/");
+// };
 
-export default handler;
+// export default handler;

44819614be987c7cdc3bbafcd5a3a6ef69a6805c
ShikshaFinder <janiharsh794@gmail.com>
2024-10-04 17:38:45 +0530

    h

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -15,21 +15,27 @@ import { Heading } from "@chakra-ui/react";
 import { Icon } from "@chakra-ui/react";
 import { Link } from "@chakra-ui/react";
 
-
-
 export default function LargeWithLogoCentered() {
   return (
     <>
       <Box bg="white" shadow="sm" p={6} rounded="lg" flex="1">
-        <Heading as="h3" size="lg">
+        <Heading as="h3" size="lg" textAlign="center">
           GTU Ventures
         </Heading>
-        <Text mt={4}>GTU,AIC</Text>
-        <Text mt={2}>Phone: (123) 456-7890</Text>
-        <Text mt={2}>Email: info@gtuventures.com</Text>
-        <Heading as="h3" size="lg" mt={8}>
+        <Text mt={4} textAlign="center">
+          GTU, AIC
+        </Text>
+        <Text mt={2} textAlign="center">
+          Phone: (123) 456-7890
+        </Text>
+        <Text mt={2} textAlign="center">
+          Email: info@gtuventures.com
+        </Text>
+
+        <Heading as="h3" size="lg" mt={8} textAlign="center">
           Follow Us
         </Heading>
+
         <Flex mt={4} gap={4} justify="center">
           <Link href="#">
             <Icon as={FiTwitter} boxSize={6} />
@@ -42,10 +48,16 @@ export default function LargeWithLogoCentered() {
           </Link>
         </Flex>
       </Box>
-      <Box as="footer" bg="gray.100" py={6} color="gray.600">
-        <Flex justify="space-between" px={4}>
+
+      <Box as="footer" bg="gray.100" py={6} color="gray.600" mt={8}>
+        <Flex
+          justify="space-between"
+          align="center"
+          px={4}
+          direction={{ base: "column", md: "row" }}
+        >
           <Text>&copy; 2024 GTU Ventures. All rights reserved.</Text>
-          <Flex gap={4}>
+          <Flex gap={4} mt={{ base: 4, md: 0 }}>
             <Link href="#">Privacy Policy</Link>
             <Link href="#">Terms of Service</Link>
           </Flex>
